fe3d322be76f03f14893c8b013223cd4d844bb67
Brian Toro <brianhorrors@hotmail.com>
2020-05-28 23:33:00 -0500

    Añadi control de errores

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const { config } = require("./config/index.js");
 const cors = require("cors");
 const { projectsAPI } = require("./controllers/routes/routeProjects");
 const { blogAPI } = require('./controllers/routes/routeBlog');
+const { logErrors, wrapErrors ,errorHandler } = require('./controllers/middlewares/errorHandler');
+const { notFoundHandler } = require('./controllers/middlewares/notFoundHandler')
 
 //Inicio del servidor
 const app = express();
@@ -16,6 +18,14 @@ app.use(express.json());
 //Routes controller
 projectsAPI(app);
 blogAPI(app);
+
+//Catch errors
+app.use(logErrors);
+app.use(wrapErrors)
+app.use(errorHandler);
+
+//Catch not found
+app.use(notFoundHandler)
 
 app.listen(config.port, () => {
     console.log(`Server listen on port: ${config.port}`);
